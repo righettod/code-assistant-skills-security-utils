f64f1de04746abec81efebbd77007bd93ee3650e
righettod <dominique.righetto@gmail.com>
2026-04-12 09:32:17 +0200

    add log forging skill

--- ci.sh
+++ ci.sh
@@ -15,3 +15,12 @@ do
     echo "[+] Validate skill file: $skill_file" 
     pyenv/bin/agentskills validate $skill_file
 done
+# Generate the skills descriptor
+skills_descriptor="available_skills.xml"
+skills_folders=""
+for skill_folder in $(ls $skills_base_folder)
+do
+    skills_folders="$skills_base_folder/$skill_folder $skills_folders"
+done
+pyenv/bin/agentskills to-prompt "$skills_folders" > $skills_descriptor
+cat $skills_descriptor
